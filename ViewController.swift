9736b1576ca34f326253be063f48a3804a65fb8c
PilyaRC <61076782+PilyaRC@users.noreply.github.com>
2021-03-16 16:19:36 +0200

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by PiliavskyiMBP on 16.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// First commit. Let's started!
+   // check
+    
+}
+
